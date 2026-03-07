1cc16b84995f0aa7c0d062691ddcaf9d2e0b50f8
GauriLakshmi <gauriilakshmii@gmail.com>
2026-03-07 23:36:22 +0530

    Deployed backend link added

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,7 @@
 import axios from "axios";
 
-// Base URL of your backend
-const BASE_URL = "http://localhost:5000/api"; // change this if deployed
-
 const API = axios.create({
-  baseURL: BASE_URL,
-});
-
-// Optional: attach token automatically for protected routes
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
+  baseURL: "https://e-commerce-backend-78el.onrender.com/api",
 });
 
 export default API;
